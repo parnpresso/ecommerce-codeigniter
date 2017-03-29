b19e008f537bb6b9995e0c39f3c8b5fbefe283c8
beamhippo <beamhippo@gmail.com>
2017-03-30 00:07:19 +0700

    aa
    aa

--- application/views/order_view.php
+++ application/views/order_view.php
@@ -1,4 +1,4 @@
-<?php //var_dump($order); ?>
+<?php var_dump($order); ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -36,7 +36,7 @@
         </div>
         <div class="col-md-9">
           <br>
-          ANSWER1<br>
+          บริษัท เอเชียการไฟฟ้า จำกัด<br>
           ANSWER2<br>
 
           <br>
