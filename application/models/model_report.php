f125137d86cd51b22d5e76c8ed12f0c3b02453b5
parnpresso <parnpresso@gmail.com>
2017-04-10 15:45:41 +0700

    Remove comment

--- application/models/model_report.php
+++ application/models/model_report.php
@@ -2,7 +2,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Model_report extends CI_Model {
+
   public function get_report($month, $year) {
+
     // Get All ordered product order by category and name
     $this->db->select('*');
     $this->db->from('order');
@@ -15,27 +17,15 @@ class Model_report extends CI_Model {
     $query = $this->db->get();
     $original = $query->result();
     $temp = $query->result();
-    //var_dump($original);
 
     // Store product
     for ($x = 1; $x < sizeof($original); $x++) {
       if ($temp[$x]->product_name == $temp[$x-1]->product_name) {
-        //$temp[$x-1] = (int)$temp[$x-1]->product_quantity + (int)$temp[$x]->product_quantity;
-        //unset($temp[$x]);
-        //var_dump($temp[$x-1]->product_name );
-        //var_dump($temp[$x]->product_name );
-        //echo $temp[$x]->product_name == $temp[$x-1]->product_name;
         $temp[$x]->product_quantity = (int)$temp[$x-1]->product_quantity + (int)$temp[$x]->product_quantity;
-        //echo $temp[$x]->product_quantity."+".$temp[$x]->product_quantity."=";
-        //echo $temp[$x]->product_quantity ;
         unset($temp[$x-1]);
-      } //else {echo "S";}
-      //echo '=============='.$x.' of '. sizeof($original).'==============<br>';
+      }
     }
     $final = array_values($temp);
-    //array_unshift($temp);
-    //$final = array_values($temp);
-    //var_dump($final);
 
     if ($final != NULL) {
       // Separate category
@@ -50,19 +40,14 @@ class Model_report extends CI_Model {
         }
       }
 
-      //var_dump($final_with_category);
-      //break;
-
-      // return $final;
       return $final_with_category;
     } else {
       return NULL;
     }
 
   }
-
-
   public function get_report_popular($month, $year) {
+
     // Get All ordered product order by category and name
     $this->db->select('*');
     $this->db->from('order');
@@ -75,22 +60,13 @@ class Model_report extends CI_Model {
     $query = $this->db->get();
     $original = $query->result();
     $temp = $query->result();
-    //var_dump($original);
 
     // Store product
     for ($x = 1; $x < sizeof($original); $x++) {
       if ($temp[$x]->product_name == $temp[$x-1]->product_name) {
-        //$temp[$x-1] = (int)$temp[$x-1]->product_quantity + (int)$temp[$x]->product_quantity;
-        //unset($temp[$x]);
-        //var_dump($temp[$x-1]->product_name );
-        //var_dump($temp[$x]->product_name );
-        //echo $temp[$x]->product_name == $temp[$x-1]->product_name;
         $temp[$x]->product_quantity = (int)$temp[$x-1]->product_quantity + (int)$temp[$x]->product_quantity;
-        //echo $temp[$x]->product_quantity."+".$temp[$x]->product_quantity."=";
-        //echo $temp[$x]->product_quantity ;
         unset($temp[$x-1]);
-      } //else {echo "S";}
-      //echo '=============='.$x.' of '. sizeof($original).'==============<br>';
+      } 
     }
     $final = array_values($temp);
     //array_unshift($temp);
@@ -134,7 +110,7 @@ class Model_report extends CI_Model {
             $temp = $final[$y-1];
             $final[$y-1] = $final[$y];
             $final[$y] = $temp;
-          } 
+          }
         }
       }
       //var_dump($final);
